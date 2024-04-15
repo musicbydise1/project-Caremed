66f64f4cf581b1952e0efe5d7272eca044d4b9b6
aksultanyessenbay <aksultane912@gmail.com>
2024-04-15 15:42:08 +0500

    15.04.2024

--- server/app.js
+++ server/app.js
@@ -38,11 +38,11 @@ app.use('/admin/dashboard', dashboardRoutes);
 app.use('/admin/users', usersRoutes);
 
 // Обработка несуществующих маршрутов
-app.use('*', (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        errors: [{ msg: 'Route not found' }],
-    });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешить CORS для всех доменов
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
 // Middleware обработки ошибок
